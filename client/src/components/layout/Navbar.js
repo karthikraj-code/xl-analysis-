0a85f4435a0790e9a5034a551510871d5833f3f3
karthikraj-code <aradhulakarthik@gmail.com>
2025-05-29 13:01:20 +0530

    Initial commit

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -21,7 +21,7 @@ const Navbar = () => {
           <div className="flex">
             <div className="flex-shrink-0 flex items-center">
               <Link to="/" className="text-xl font-bold text-gray-800">
-                XL Analysis
+                EXCEL ANALYSIS PLATFORM
               </Link>
             </div>
             {isAuthenticated && (
